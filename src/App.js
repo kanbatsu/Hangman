6e18c5ed81a759e8b7a99af7adb8b0be14f6ed6b
Kanbatsu <ludwig.secheresse@gmail.com>
2019-04-17 09:36:50 +0200

    Start keyboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Keyboard from './Keyboard'
+
 class App extends Component {
   render() {
     return (
@@ -11,14 +13,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Keyboard/>
         </header>
       </div>
     );
---
+++ src/Keyboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Keyboard.css'
+
+const Keyboard = (keys) => (
+    <div>
+        Ici y aura le clavier
+    </div>
+)
+
+export default Keyboard
